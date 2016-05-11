5195ceb6c9e056e568d02185c64ebb76364b4474
Greg Duncan <mail@gregduncan.co.uk>
2016-05-11 11:17:58 +0100

    init

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Bootstrap from 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+import '../../css/styles.css!';
+
+class Calentics extends React.Component{
+
+	render(){
+		return <p>Welcome to Calentics</p>
+	}
+}
+
+ReactDom.render(<Calentics />, document.getElementById('app'))
